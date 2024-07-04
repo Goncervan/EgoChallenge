00741f7f66c336d56f4b73189a662b6c87b8da58
Gonzalo Cervan <gcervan@globalthinktec.com>
2024-07-04 07:58:47 -0300

    feat/menu;

--- ego-challenge/src/components/navbar/navbar.tsx
+++ ego-challenge/src/components/navbar/navbar.tsx
@@ -1,21 +1,25 @@
-'use client'
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.styles.scss";
 import { EgoIcon } from "../icons/EgoIcon";
 import { usePathname } from "next/navigation";
 import { MenuIcon } from "../icons/MenuIcon";
+import { Menu } from "../menu/Menu";
 export const Navbar = () => {
   const pathname = usePathname();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen((prev) => !prev);
+  };
   return (
     <nav className="nav">
       <div className="left_container">
         <EgoIcon />
         <div className="links_container">
-          <Link
-            className={pathname === "/" ? "active_link" : "link"}
-            href="/"
-          >
+          <Link className={pathname === "/" ? "active_link" : "link"} href="/">
             Modelos
           </Link>
           <Link
@@ -26,7 +30,10 @@ export const Navbar = () => {
           </Link>
         </div>
       </div>
-      <button className="menu">Menú <MenuIcon/></button>
+      <button className="menu" onClick={toggleModal}>
+        Menú <MenuIcon />
+      </button>
+      <Menu isOpen={isModalOpen} onClose={toggleModal}/>
     </nav>
   );
 };
